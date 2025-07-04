957db584b3450df92d56b1a2b6bf8af14a1f51a7
Gowthami264 <ugowthami04@gmail.com>
2025-07-04 17:23:55 +0530

    Update BookRepository.java

--- src/main/java/com/library/repository/BookRepository.java
+++ src/main/java/com/library/repository/BookRepository.java
@@ -1,7 +1,6 @@
 package com.library.repository;
 
-import org.springframework.data.repository.CrudRepository;
 import com.library.model.Book;
+import org.springframework.data.repository.CrudRepository;
 
-public interface BookRepository extends CrudRepository<Book, Long> {
-}
+public interface BookRepository extends CrudRepository<Book, Long> {}
