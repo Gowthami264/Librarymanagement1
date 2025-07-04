47ff6828530a51702d961c02786196fb85c58d1d
Gowthami264 <ugowthami04@gmail.com>
2025-07-04 17:23:31 +0530

    Update Book.java

--- src/main/java/com/library/model/Book.java
+++ src/main/java/com/library/model/Book.java
@@ -17,8 +17,8 @@ public class Book {
     }
 
     public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
     public String getTitle() { return title; }
+
+    public void setId(Long id) { this.id = id; }
     public void setTitle(String title) { this.title = title; }
 }
